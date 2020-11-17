be799b6b04f0601bd6be7c0d0b7d8e47ca376c00
clement0010 <clement0010@hotmail.com>
2020-11-18 02:15:01 +0800

    add apollo caching service

---
+++ src/components/organisms/NavBarWeb.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-toolbar-title>
+          Cyberia 2020 Site
+        </v-toolbar-title>
+      </div>
+
+      <v-spacer />
+
+      <router-link
+        to="/"
+        class="text-decoration-none"
+      >
+        <v-btn class="mx-1">
+          Home
+        </v-btn>
+      </router-link>
+      <router-link
+        to="/profile"
+        class="text-decoration-none"
+      >
+        <v-btn class="mx-1">
+          Profile
+        </v-btn>
+      </router-link>
+      <router-link
+        to="/"
+        class="text-decoration-none"
+      >
+        <v-btn class="mx-1">
+          Login
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'NavBarWeb',
+
+  components: {
+  },
+
+  data: () => ({
+    drawer: false,
+  }),
+});
+</script>
---
+++ src/views/InformationPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <v-container
+      class="mx-6 px-5"
+      fluid
+    >
+      <h1 class="text-center">
+        Information Page
+      </h1>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <p>
+            Cyberia 2020 is an annual Adobe Photoshop boot camp organized by NUSSU commIT, with the
+            aim of imparting graphic design knowledge and skills to NUS undergraduates to strengthen
+            their skill set. You will learn basic to intermediate Adobe Photoshop and their
+            practical applications through trainings by certified professional Adobe Photoshop
+            trainer and a series of fun and exciting games. At the end of the camp, you will have
+            the opportunity to put all the skills you learn to test by coming out an artwork in a
+            competition and win attractive prizes!
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'About',
+  components: {
+  },
+});
+</script>
