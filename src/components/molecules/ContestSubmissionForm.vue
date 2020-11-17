4b4c9fe6e64d151e52df05484c65c0d2d5d444df
clement0010 <clement0010@hotmail.com>
2020-11-18 04:34:14 +0800

    integrate file submission system to backend

--- src/components/molecules/ContestSubmissionForm.vue
+++ src/components/molecules/ContestSubmissionForm.vue
@@ -20,16 +20,28 @@
           <span class="headline">Add Submission</span>
         </v-card-title>
         <v-card-text>
-          <v-file-input placeholder="Upload file" />
+          <v-file-input
+            placeholder="Upload file"
+            name="imageFileSubmission"
+            show-size
+            accept="image/*"
+            @change="selectFile"
+          />
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
+            type="submit"
             color="primary"
-            @click="dialog = false"
+            @click="upload"
           >
             Submit
           </v-btn>
+          <SnackBar
+            :text="message"
+            :timeout="timeout"
+            :snackbar="snackbar"
+          />
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -37,16 +49,73 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
+import SnackBar from '@/components/atoms/Snackbars.vue';
+import uploadService from '@/services/fileSystem';
 
 export default defineComponent({
   name: 'ContestSubmissionForm',
 
   components: {
+    SnackBar,
   },
 
-  data: () => ({
-    dialog: false,
-  }),
+  setup() {
+    const dialog = ref(false);
+
+    const timeout = 2000;
+    const snackbar = ref(false);
+    const message = ref('');
+    function snackbarHandler(text: string) {
+      console.log(snackbar.value);
+      if (!snackbar.value) {
+        message.value = text;
+        snackbar.value = true;
+        setInterval(() => {
+          snackbar.value = false;
+        }, timeout);
+      }
+    }
+
+    const currentFile = ref();
+    const imageURL = ref('');
+    function selectFile(file: Blob) {
+      console.log('Selected File: ', file);
+      currentFile.value = file;
+    }
+
+    async function upload() {
+      if (!currentFile.value) {
+        snackbarHandler('Please select a file!');
+        return;
+      }
+
+      try {
+        const { data, status } = await uploadService(currentFile.value, 'auth0|12345678910');
+        if (status === 200) {
+          imageURL.value = data.imageURL;
+          console.log(imageURL.value, 'Image URL==============');
+          dialog.value = false;
+          return;
+        }
+        throw new Error('Submission Server Error');
+      } catch (error) {
+        if (!snackbar.value) {
+          snackbarHandler('Something is wrong. Please try again later.');
+        }
+        console.log('ERROR: ', error);
+      }
+    }
+
+    return {
+      dialog,
+      upload,
+      selectFile,
+      currentFile,
+      snackbar,
+      timeout,
+      message,
+    };
+  },
 });
 </script>
--- src/services/fileSystem.ts
+++ src/services/fileSystem.ts
@@ -1,36 +1,20 @@
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- * Note: when creating a signed URL, unless running in a GCP environment,
- * a service account must be used for authorization.
- */
+import axios from 'axios';
 
-// Imports the Google Cloud client library
-import { Storage, GetSignedUrlConfig } from '@google-cloud/storage';
-
-const bucketName = 'contest-submission';
-// Creates a client
-const storage = new Storage();
-
-export default async function generateV4SignedPolicy(filename: string) {
-  const options: GetSignedUrlConfig = {
-    version: 'v4',
-    action: 'write',
-    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-    contentType: 'application/octet-stream',
+interface UploadResponse {
+  data: {
+    imageURL: string;
   };
+  status: number;
+}
 
-  // Get a v4 signed URL for uploading file
-  const [url] = await storage
-    .bucket(bucketName)
-    .file(filename)
-    .getSignedUrl(options);
+export default async function uploadService(file: string|Blob, userId: string): Promise<UploadResponse> {
+  const formData = new FormData();
 
-  console.log('Generated PUT signed URL:');
-  console.log(url);
-  console.log('You can use this URL with any user agent, for example:');
-  console.log(
-    "curl -X PUT -H 'Content-Type: application/octet-stream' "
-   + `--upload-file my-file '${url}'`,
-  );
-  generateV4SignedPolicy('test').catch(console.error);
+  formData.append('file', file);
+
+  return axios.post(`http://localhost:3000/api/upload/${userId}`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
 }
