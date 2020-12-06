a3ead7f7644950ee06c48205d942a6a368235493
jmleong666 <ljming4848@hotmail.com>
2020-12-06 20:00:56 +0800

    Add itinerary

---
+++ src/components/molecules/DayItinerary.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-col cols="12">
+    <h2 class="text-center align-center">Day {{ day }}</h2>
+    <div class="my-5" />
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center text-subtitle-1">
+              Time
+            </th>
+            <th class="text-center text-subtitle-1">
+              Duration
+            </th>
+            <th class="text-center text-subtitle-1">
+              Program
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <td class="text-center">
+              {{ item.time }}
+            </td>
+            <td class="text-center">
+              {{ item.duration }}
+            </td>
+            <td class="text-center">
+              {{ item.program }}
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'DayItinerary',
+  components: {
+  },
+  props: {
+    day: {
+      type: String,
+      default: () => 0,
+    },
+    items: {
+      type: Array,
+      default: () => [{
+        time: '0800-0900',
+        duration: '1 hr',
+        program: 'Wake up',
+      }],
+    },
+  },
+});
+</script>
--- src/components/organisms/Itinerary.vue
+++ src/components/organisms/Itinerary.vue
@@ -1,16 +1,135 @@
 <template>
-  <div>
-    Itinerary
-  </div>
+  <v-container>
+    <day-itinerary
+      day="1"
+      :items="dayOnePrograms"
+    />
+    <day-itinerary
+      day="2"
+      :items="dayTwoPrograms"
+    />
+    <day-itinerary
+      day="3"
+      :items="dayThreePrograms"
+    />
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import DayItinerary from '../molecules/DayItinerary.vue';
 
 export default defineComponent({
   name: 'Itinerary',
 
   components: {
+    DayItinerary,
   },
+
+  data: () => ({
+    dayOnePrograms: [{
+      time: '0930 – 1000',
+      duration: '30 min',
+      program: 'Participants join zoom',
+    }, {
+      time: '1000 – 1100',
+      duration: '1 hr',
+      program: 'Opening speech + briefing',
+    }, {
+      time: '1100 – 1145',
+      duration: '45 min',
+      program: 'Ice-breakers',
+    }, {
+      time: '1145 – 1245',
+      duration: '1 hr',
+      program: 'Lunch',
+    }, {
+      time: '1245 – 1445',
+      duration: '2 hr',
+      program: 'Training',
+    }, {
+      time: '1445 - 1500',
+      duration: '15 min',
+      program: 'Break',
+    }, {
+      time: '1500 - 1700',
+      duration: '2 hr',
+      program: 'Training',
+    }, {
+      time: '1700 - 1715',
+      duration: '15 min',
+      program: 'Break',
+    }, {
+      time: '1715 - 1830',
+      duration: '1 hr 15 min',
+      program: 'Chivalry of Space',
+    }, {
+      time: '1830 - 1945',
+      duration: '1 hr 15 min',
+      program: 'Dinner',
+    }, {
+      time: '1945 – 2145',
+      duration: '2 hr',
+      program: 'Night training',
+    }, {
+      time: '2145 – 2230',
+      duration: '45 min',
+      program: 'Virtual Firecamp',
+    }],
+    dayTwoPrograms: [{
+      time: '0945 – 1000',
+      duration: '15 min',
+      program: 'Participants join zoom',
+    }, {
+      time: '1000 – 1200',
+      duration: '2 hr',
+      program: 'Training',
+    }, {
+      time: '1200 – 1300',
+      duration: '1 hr',
+      program: 'Lunch',
+    }, {
+      time: '1300 - 1500',
+      duration: '2 hr',
+      program: 'Training',
+    }, {
+      time: '1500 – 1530',
+      duration: '30 min',
+      program: 'Break',
+    }, {
+      time: '1530 - 1545',
+      duration: '15 min',
+      program: 'Competition briefing',
+    }, {
+      time: '1545 - 1845',
+      duration: '3 hr',
+      program: 'Competition',
+    }, {
+      time: '1845 - 2000',
+      duration: '1 hr 15 min',
+      program: 'Dinner',
+    }, {
+      time: '2000 - 2130',
+      duration: '1 hr 30 min',
+      program: 'Space Warfare',
+    }, {
+      time: '2145 – 2230',
+      duration: '45 min',
+      program: 'Virtual Firecamp',
+    }],
+    dayThreePrograms: [{
+      time: '0945 – 1000',
+      duration: '15 min',
+      program: 'Participants join zoom',
+    }, {
+      time: '1000 – 1030',
+      duration: '30 min',
+      program: 'Emergency meeting!',
+    }, {
+      time: '1030 – 1130',
+      duration: '1 hr',
+      program: 'Closing ceremony + Announcement of winners',
+    }],
+  }),
 });
 </script>
