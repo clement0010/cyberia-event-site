5713ce980ae0479a72014ea3c1fe34478d014378
jmleong666 <ljming4848@hotmail.com>
2020-12-06 23:39:48 +0800

    Update general information page

--- src/components/organisms/GeneralInfo.vue
+++ src/components/organisms/GeneralInfo.vue
@@ -4,22 +4,120 @@
       class="mx-6 px-5"
       fluid
     >
-      <h1 class="text-center">
-        About Cyberia 2020
-      </h1>
+      <v-row
+        justify="center"
+        class="my-5"
+      >
+        <v-img
+          src="@/assets/poster.png"
+          max-width="50vw"
+        />
+      </v-row>
       <v-row justify="center">
         <v-col
           cols="12"
-          md="6"
+          md="9"
         >
           <p>
-            Cyberia 2020 is an annual Adobe Photoshop boot camp organized by NUSSU commIT, with the
-            aim of imparting graphic design knowledge and skills to NUS undergraduates to strengthen
-            their skill set. You will learn basic to intermediate Adobe Photoshop and their
-            practical applications through trainings by certified professional Adobe Photoshop
-            trainer and a series of fun and exciting games. At the end of the camp, you will have
-            the opportunity to put all the skills you learn to test by coming out an artwork in a
-            competition and win attractive prizes!
+            Cyberia 2020 is an annual Adobe Photoshop boot camp organized by NUSSU commIT, which aims to impart
+            graphic design knowledge and skills to the participants to strengthen their skill set. You will
+            learn Adobe Photoshop with <strong>training conducted by a certified professional Adobe Photoshop trainer
+            </strong> and <strong>a series of challenging and exciting games</strong>. At the end of the camp,
+            you will have the opportunity to put all the skills you learn to test by producing an artwork in our
+            <strong>competition</strong> and stand to <strong>win attractive prizes</strong>!
+          </p>
+          <p>
+            <strong>As a participant, you will receive:</strong>
+            <ul>
+              <li>
+                Comprehensive Adobe Photoshop training + Certificate <strong>(worth $450)</strong>
+              </li>
+              <li>
+                Camp T-shirt
+              </li>
+              <li>
+                Goodie bag
+              </li>
+            </ul>
+          </p>
+          <p>
+            <strong>Details of Cyberia 2020:</strong>
+            <ul>
+              <li>
+                <strong>Date: </strong> 7th - 9th December 2020
+              </li>
+              <li>
+                <strong>Camp fee: </strong> $30 <strong>(worth $450)</strong>
+              </li>
+              <li>
+                <strong>Venue: </strong> Fully online
+              </li>
+            </ul>
+          </p>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="9"
+        >
+          <h1>About Acadia Training</h1>
+          <div class="my-5" />
+          <p>
+            <a href="https://www.acadia.sg/">Acadia Training Pte Ltd</a> was established in July 2012.
+            Our main focus is in corporate software training, especially in graphic and web design.
+          </p>
+          <p>
+            Since day one, we have been an official Training Partner of Adobe System Inc. All our classes are
+            conducted by Adobe Certified Instructors (ACI), trainers who are certified subject matter experts
+            and appointed by Adobe to conduct authorised training for Adobe software. Our trainers are known
+            for their patience, friendly disposition as well as their willingness to provide help to their
+            participants after the course.
+          </p>
+          <p>
+            Although we are a newly-formed company, our team is made up of veterans who have been in the training
+            and design industry with at least 30 years combined experience.
+          </p>
+          <p>
+            At Acadia Training, our objective is to deliver quality and up-to-date IT courses that are relevant to
+            current industry needs and to provide an enriching learning experience for all our customers.
+          </p>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="9"
+        >
+          <h1>About the trainer: Ricky Soh</h1>
+          <div class="my-5" />
+          <p>
+            A self-professed technophile, Ricky’s fascination for computer software developed into a passion for
+            sharing his knowledge in IT With more than 20 years in the IT training industry, Ricky’s extensive
+            experience comes from teaching as a polytechnic lecturer and corporate instructor.
+          </p>
+          <p>
+            At Ngee Ann Polytechnic, Ricky taught programming languages as well as web design and web development. As
+            a corporate instructor, Ricky has been an Adobe Certified Instructor and Adobe Certified Expert since 2006.
+            Due to his numerous Adobe certifications, he has been conferred the highly coveted title of “Adobe Certified
+            Instructor – Creative Suite Master”. Some of his major clients include GlaxoSmithKline, NOL, AIA, SPH, and
+            Seagate. He has also collaborated with People’s Association in organising and conducting IT courses for the
+            public in community clubs.
+          </p>
+          <p>
+            With his diverse experience and broad knowledge, Ricky is an excellent facilitator whose previous participants
+            have greatly benefitted from his insights. Besides understanding the needs of his participants, Ricky’s
+            strengths are his clear, focussed delivery and his ability to generate enthusiasm. His affable personality
+            and cheerful style means his lessons are always interesting and engaging.
+          </p>
+          <p>
+            In September 2016, he obtained his Advanced Certificate in Training and Assessment (ACTA). It is a programme
+            under the Singapore Workforce Skills Qualifications (WSQ) that certifies the recipient with the essential
+            skills to deliver training and assessments across different industries.
+          </p>
+          <p>
+            Ricky is also a paper craft designer and author/publisher of two book titles that are currently being
+            retailed in all four branches of Kinokuniya Book Stores.
           </p>
         </v-col>
       </v-row>
