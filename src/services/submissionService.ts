9e580eb8145d9041b56d1d974abc9841c869f66b
clement0010 <clement0010@hotmail.com>
2020-12-07 09:40:00 +0800

    fix submission system

--- src/services/submissionService.ts
+++ src/services/submissionService.ts
@@ -12,7 +12,7 @@ export default async function uploadService(file: string|Blob): Promise<UploadRe
 
   formData.append('file', file);
 
-  return axios.post(`${process.env.VUE_APP_FILE_SUBMISSION_URL}api/upload/`, formData, {
+  return axios.post(`${process.env.VUE_APP_FILE_SUBMISSION_URL}/api/upload/`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
       'Access-Control-Allow-Origin': '*',
